93c6c5a7c068332c2e41877bcc1be1c79c95f175
liuzhenxin <liuzhenxin1977@gmail.com>
2025-10-28 10:29:45 +0800

    完成基本组件环境

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// global css
+import 'virtual:uno.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import '@/assets/styles/index.scss'
+
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// 高亮组件
+// import 'highlight.js/styles/a11y-light.css';
+import 'highlight.js/styles/atom-one-dark.css'
+import 'highlight.js/lib/common'
+import HighLight from '@highlightjs/vue-plugin'
 
-app.use(createPinia())
+// svg图标
+import 'virtual:svg-icons-register'
+import ElementIcons from '@/plugins/svgicon'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(HighLight)
+app.use(ElementIcons)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
